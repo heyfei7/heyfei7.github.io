9a1941b5da20683e9a38d648636dc85eb183b8b8
heyfei7 <feidong1998@gmail.com>
2021-01-12 16:33:19 -0500

    Added support for multiple views

--- src/App.js
+++ src/App.js
@@ -1,103 +1,49 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// Views
+import { getData } from './common/util';
+import DefaultView from "./views/default/Default"
+import FiveStarsView from "./views/five-stars/FiveStars"
+
+// React-Bootstrap
+import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
+import Dropdown from 'react-bootstrap/Dropdown';
 
-import { Envelope, Github, Linkedin, TelephoneFill } from 'react-bootstrap-icons';
-
-
-function Section(props) {
+function ViewToggle(props) {
   return (
-    <Card className="mb-4">
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        {props.children}
-      </Card.Body>
-    </Card>
-  )
-}
-
-function AboutMe(props) {
-  return (
-    <Section title="Fei Dong">
-      Web Developer
-      Software Programming
-      Writer
-    </Section>
-  )
-}
-
-function WorkExperience(props) {
-  return (
-    <Section title="Work Experience">
-
-    </Section>
-  )
-}
-
-function MySkills(props) {
-  return (
-    <Section title="Skills">
-
-    </Section>
-  )
-}
-
-function Education(props) {
-  return (
-    <Section title="Education">
-
-    </Section>
-  )
-}
-
-function Contact(props) {
-  return (
-    <div>
-      <div>{props.icon}</div>
-      <p>{props.title}</p>
-      <p>{props.value}</p>
-    </div>
-  )
-}
-
-function ContactMe(props) {
-  const contacts = [
-    { "title": "E-Mail", "value": "feidong1998@gmail.com", "icon": <Envelope /> },
-    { "title": "Phone", "value": "(647) 233-3048", "icon":<TelephoneFill/> },
-    { "title": "LinkedIn", "value": "@heyfei7", "link": "https://www.linkedin.com/in/heyfei7", "icon":<Linkedin/>},
-    { "title": "GitHub", "value": "@heyfei7", "link": "https://github.com/heyfei7", "icon":<Github/> }
-  ]
-  const cols = [];
-
-  for (let contact of contacts) {
-    cols.push(
-      <Col xs={6} md={3}>
-        <Contact title={contact.title} value={contact.value} link={contact.link} icon={contact.icon}></Contact>
-      </Col>)
-  }
-
-  return (
-    <Section title="Contact Me">
-      <Container><Row>{cols}</Row></Container>
-      {/* write a thing to send auto-emails?? */}
-    </Section>
+    <Dropdown >
+      <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+        View
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Dropdown.Item onClick={() => props.setView("default")}>Default</Dropdown.Item>
+        <Dropdown.Item onClick={() => props.setView("five-stars")}>Five Stars</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
   )
 }
 
 function App() {
+  const [view, setView] = useState("five-stars");
+  const [data, setData] = useState({});
+  const getAllData = async () => {
+    getData("contacts.json", (contacts) => {
+      getData("experience.json", (exp) => {
+        getData("skills.json", (skills) => {
+          setData(Object.assign(contacts, exp, skills));
+        })
+      })
+    })
+  }
+
+  useEffect(() => { getAllData(); }, []);
   return (
     <Container className="p-3">
-      <AboutMe />
-      <WorkExperience />
-      <MySkills />
-      <Education />
-      <ContactMe />
+      {
+        (view == "default" && <DefaultView data={data} />) ||
+        (view == "five-stars" && <FiveStarsView data={data} />)
+      }
     </Container>
   );
 }
---
+++ src/views/five-stars/FiveStars.js
@@ -0,0 +1,199 @@
+
+import "./FiveStars.css"
+import { getIcon } from '../../common/util.js';
+
+// React-Bootstrap
+import { useState } from 'react';
+import { Row, Col, Button, Carousel, Badge, Collapse } from 'react-bootstrap';
+import { CaretDownFill, CaretUpFill, List, Star, StarFill } from 'react-bootstrap-icons';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image'
+import Card from 'react-bootstrap/Card'
+
+function Header(props) {
+    return (
+        <Card id="about-me" className="five-stars section">
+            <Card.Header>
+                <Row>
+                    <Col xs={3} md={3}>
+                        <Image
+                            src={process.env.PUBLIC_URL + "/images/icon.jpeg"}
+                            style={{ "width": "inherit", "height": "inherit" }}
+                            rounded />
+                    </Col>
+                    <Col xs={9} md={9}>
+                        <h1>Fei Dong</h1>
+                        <p>Web Developer | Software Programmer | Writer</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={6}><Button>Contact Me</Button></Col>
+                    <Col xs={6}></Col>
+                </Row>
+            </Card.Header>
+        </Card>
+    )
+}
+
+function ToggleSection(props) {
+    const [show, setShow] = useState(false);
+    return (
+        <Card
+            id={props.id}
+            className="five-stars section toggle-section">
+            <Card.Header
+                className="text-center pointer">
+                <Row
+                    onClick={() => setShow(!show)}
+                    aria-controls={props.id + "-body"}
+                    aria-expanded={show}>
+                    <Col xs={9}>{props.title}</Col>
+                    <Col xs={3}>
+                        {show && <CaretUpFill />}
+                        {!show && <CaretDownFill />}
+                    </Col>
+                </Row>
+            </Card.Header>
+            <Collapse in={show}>
+                <Card.Body id={props.id + "body"}>
+                    {props.children}
+                </Card.Body>
+            </Collapse>
+        </Card>
+    )
+}
+
+function AboutMe(props) {
+    return (
+        <ToggleSection id="about-me" title="About Me">
+            <div>lorem ipsum</div>
+        </ToggleSection>
+    )
+}
+
+function Experience(props) {
+    const ExpItem = (item) => {
+        console.log("helloworld?");
+        return (
+            <Carousel.Item>
+                {console.log("whoop", item)}
+                <img
+                    className="d-block w-100"
+                    src="holder.js/800x400?text=First slide&bg=373940"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        )
+    }
+
+    return (
+        <ToggleSection id="my-experience" title="Experience">
+            <Carousel>
+                {
+                    props.experience &&
+                    props.experience > 0 &&
+                    props.experience.map(ExpItem)
+                }
+            </Carousel>
+        </ToggleSection>
+    )
+}
+
+
+function MySkills(props) {
+    const [state, setState] = useState({ "frontend": true, "backend": true, "os": false, "others": false, "database": false });
+
+    const setKeyValue = (key, value) => {
+        var tmpState = {};
+        tmpState[key] = value;
+        setState((Object.assign({}, state, tmpState)));
+    }
+
+    const Stars = (props) => {
+        return (
+            <div>
+                {
+                    [...Array(props.level).keys()].map((key) => {
+                        return <StarFill key={key} />
+                    })
+                }
+            </div>
+        )
+    }
+
+    const SkillItem = (item) => {
+        if (state[item.type]) {
+            return (
+                <Col xs={4} md={3} key={item.name} className="text-center" >
+                    {item.name}
+                    <Stars level={item.level} />
+                </Col>
+            )
+        }
+    }
+
+    return (
+        <ToggleSection id="my-skills" title="Skills">
+            {
+                Object.entries(state).map(([key, value]) => {
+                    return (
+                        <Badge pill
+                            className="pointer"
+                            key={key}
+                            onClick={() => setKeyValue(key, !value)}
+                            variant={value ? "info" : "light"}>{key}</Badge>
+                    )
+                })
+            }
+            <Row>
+                {
+                    props.skills &&
+                    props.skills.length > 0 &&
+                    props.skills.map(SkillItem)
+                }
+            </Row>
+        </ToggleSection >
+    )
+}
+
+function ContactMe(props) {
+    const ContactItem = (item) => {
+        return (
+            <Row key={item.title}>
+                <Col xs={1}>{getIcon(item.icon)}</Col>
+                <Col xs={11}>
+                    <b>{item.title}</b><br />
+                    {item.value}
+                </Col>
+            </Row>
+        )
+    }
+
+    return (
+        <ToggleSection id="contact-me" title="Contact Me">
+            {
+                props.contacts &&
+                props.contacts.length > 0 &&
+                props.contacts.map(ContactItem)
+            }
+        </ToggleSection>
+    )
+}
+
+function FiveStarsView(props) {
+    return (
+        <Container>
+            <Header />
+            <AboutMe />
+            <Experience experience={props.data.experience} />
+            <MySkills skills={props.data.skills} />
+            <ContactMe contacts={props.data.contacts} />
+        </Container>
+    )
+}
+
+export default FiveStarsView;
