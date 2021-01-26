01524a78639735fc08b877f7410f0596ee28a41d
heyfei7 <feidong1998@gmail.com>
2021-01-26 15:55:14 -0500

    Added submit button to StarryNightControl

--- src/views/starry-night/StarryNight.js
+++ src/views/starry-night/StarryNight.js
@@ -6,128 +6,9 @@ import React, { useState } from 'react';
 import { Link } from "react-scroll";
 
 // React Bootstrap
-import { Row, Col, Card, Form, Container } from 'react-bootstrap';
+import { Row, Col, Card, Button, Container } from 'react-bootstrap';
 import { Sliders, StarFill } from 'react-bootstrap-icons';
 // import Container from 'react-bootstrap/Container';
-
-class Star {
-    constructor(baseX, baseY, size, color) {
-        this.baseX = baseX;
-        this.baseY = baseY;
-        this.x = baseX;
-        this.y = baseY;
-        this.size = size;
-        this.color = color || "white";
-        this.className = "star fade-out " + (Math.random() < 0.5 ? "twinkle1" : "twinkle2")
-    }
-
-    style() {
-        return {
-            position: "absolute",
-            top: this.y,
-            left: this.x,
-            height: this.size.toString() + "px",
-            width: this.size.toString() + "px",
-            color: this.color
-        }
-    }
-
-    get() {
-        return <StarFill className={this.className} key={[this.x, this.y].toString()} style={this.style()} />
-    }
-}
-
-function jitter(n, delta) {
-    return n + delta * (Math.random() * 2 - 1);
-}
-
-class StarryNight extends React.Component {
-
-    static getInterval(freq) {
-        var interval = 20;
-        if (freq == "less") {
-            interval = 80;
-        } else if (freq == "medium") {
-            interval = 40;
-        }
-        return interval;
-    }
-
-    static getStars(height, width, freq) {
-        var stars = [];
-        const interval = StarryNight.getInterval(freq);
-        for (var y = 0; y < height; y += interval) {
-            for (var x = 0; x < width; x += interval) {
-                stars.push(new Star(y, x, 0.5));
-            }
-        }
-        return stars;
-    }
-
-    jitterStarPosition(freq, jitterPosition) {
-        const interval = StarryNight.getInterval(freq);
-        const deltaPosition = 0.70 * interval * jitterPosition + 0.25 * interval;
-        this.stars = this.stars.map(star => {
-            star.x = jitter(star.baseX, deltaPosition)
-            star.y = jitter(star.baseY, deltaPosition)
-            return star;
-        })
-    }
-
-    scaleStarSize(starSize) {
-        /*const deltaSize = jitterSize / 2;*/
-        const baseSize = starSize * 2 + 0.25;
-        this.stars = this.stars.map(star => {
-            star.size = baseSize + jitter(1, baseSize);
-            return star;
-        })
-    }
-
-    getStars(width, height, freq, jitterPosition, starSize) {
-        console.log(freq);
-        this.stars = StarryNight.getStars(width, height, freq);
-        this.jitterStarPosition(freq, jitterPosition);
-        this.scaleStarSize(starSize);
-    }
-
-    constructor(props) {
-        super(props);
-        this.updateTimer = null;
-        this.lastUpdate = Date.now();
-        this.getStars(props.state.width, props.state.height,
-            props.state.freq,
-            props.state.jitterPosition, props.state.starSize);
-    }
-
-    updateStars(prevState, state) {
-        if (prevState.jitterPosition != state.jitterPosition) {
-            this.jitterStarPosition(state.freq, state.jitterPosition);
-        }
-        if (prevState.starSize != state.starSize) {
-            this.scaleStarSize(state.starSize);
-        }
-        if (prevState.freq != state.freq) {
-            this.getStars(state.width, state.height,
-                state.freq,
-                state.jitterPosition, state.starSize);
-        }
-    }
-
-    componentDidUpdate(prevProps) {
-        this.updateStars(prevProps.state, this.props.state);
-        if (this.props.userID !== prevProps.userID) {
-            this.fetchData(this.props.userID);
-        }
-    }
-
-    render() {
-        return (
-            <Container id="starry-night" className={this.props.state.twinkle ? "twinkle" : ""} fluid>
-                {this.stars.map(x => x.get())}
-            </Container>
-        )
-    }
-}
 
 const controls = {
     twinkle: {
@@ -153,80 +34,215 @@ const controls = {
     }
 }
 
+class Star {
+    constructor(baseX, baseY, size, color) {
+        this.baseX = baseX;
+        this.baseY = baseY;
+        this.x = baseX;
+        this.y = baseY;
+        this.size = size;
+        this.color = color || "white";
+        this.className = "star fade-out " + (Math.random() < 0.5 ? "twinkle1" : "twinkle2")
+    }
+
+    style() {
+        return {
+            position: "absolute",
+            top: this.y,
+            left: this.x,
+            height: this.size.toString() + "px",
+            width: this.size.toString() + "px",
+            color: this.color
+        }
+    }
+
+    get() {
+        return <StarFill
+            className={this.className}
+            key={[this.x, this.y].toString()}
+            style={this.style()} />
+    }
+}
+
+class StarryNight extends React.Component {
+
+    static jitter(n, delta) {
+        return n + delta * (Math.random() * 2 - 1);
+    }
+
+    static getInterval(freq) {
+        var interval = 20;
+        if (freq == "less") {
+            interval = 80;
+        } else if (freq == "medium") {
+            interval = 40;
+        }
+        return interval;
+    }
+
+    static getStars(height, width, freq) {
+        var stars = [];
+        const interval = StarryNight.getInterval(freq);
+        for (var y = 0; y < height; y += interval) {
+            for (var x = 0; x < width; x += interval) {
+                stars.push(new Star(y, x, 0.5));
+            }
+        }
+        return stars;
+    }
+
+    static jitterStarPosition(stars, freq, jitterPosition) {
+        const interval = StarryNight.getInterval(freq);
+        const deltaPosition = 0.70 * interval * jitterPosition + 0.25 * interval;
+        stars.map(star => {
+            star.x = StarryNight.jitter(star.baseX, deltaPosition)
+            star.y = StarryNight.jitter(star.baseY, deltaPosition)
+            return star;
+        })
+    }
+
+    static scaleStarSize(stars, starSize) {
+        const baseSize = starSize * 2 + 0.25;
+        stars.map(star => {
+            star.size = baseSize + StarryNight.jitter(1, baseSize);
+            return star;
+        })
+    }
+
+    constructor(props) {
+        super(props);
+        this.updateStars();
+    }
+
+    updateStars() {
+        const { width, height, freq, jitterPosition, starSize } = this.props.config;
+        this.stars = StarryNight.getStars(width, height, freq);
+        StarryNight.jitterStarPosition(this.stars, freq, jitterPosition);
+        StarryNight.scaleStarSize(this.stars, starSize);
+    }
+
+    render() {
+        this.updateStars();
+        return (
+            <Container id="starry-night" className={this.props.config.twinkle ? "twinkle" : ""} fluid>
+                {this.stars.map(x => x.get())}
+            </Container>
+        )
+    }
+}
+
 function StarryNightControl(props) {
+    const [form, _setForm] = useState(props.state);
+    const setForm = (key, value) => {
+        _setForm((prevState) => Object.assign({}, prevState, ({ [key]: value })));
+    }
+
+    console.log("form", form);
+
+    const RangeControl = (props) => {
+        return (
+            <input
+                min={0} max={1}
+                value={form[props.name]}
+                type="range"
+                step={0.05}
+                onChange={(event) => {
+                    event.preventDefault();
+                    setForm(props.name, parseFloat(event.target.value));
+                }}
+            />
+        )
+    }
+
+    const CheckboxControl = (props) => {
+        return (
+            <input
+                type="checkbox"
+                checked={form[props.name]}
+                onChange={(event) => {
+                    event.preventDefault();
+                    setForm(props.name, event.target.checked);
+                }}
+                readOnly />
+        )
+    }
+
+    const SelectControl = (props) => {
+        const item = controls[props.name]
+        return (
+            <select
+                value={form[props.name]}
+                onChange={(event) => {
+                    event.preventDefault();
+                    setForm(props.name, event.target.value);
+                }}>
+                {item.options.map((x, i) => <option key={i} value={x}>{x}</option>)}
+            </select>
+        )
+    }
+
+    const ControlRow = (props) => {
+        const item = controls[props.name];
+        const id = "starry-night-control-" + props.name;
+        return (
+            <Row>
+                <Col><label htmlFor={id}>{item.name}</label></Col>
+                <Col>
+                    {item.type == "range" && <RangeControl id={id} name={props.name} />}
+                    {item.type == "checkbox" && <CheckboxControl id={id} name={props.name} />}
+                    {item.type == "select" && <SelectControl id={id} name={props.name} />}
+                </Col>
+            </Row>
+        )
+    }
+
     return (
-        < Card id="starry-night-controls" >
+        <Card id="starry-night-controls" >
             <Card.Body>
-                {Object.entries(controls).map(([key, item]) => {
-                    const id = "starry-night-control-" + key;
-                    return (
-                        <Row key={key}>
-                            <Col><label htmlFor={id}>{item.name}</label></Col>
-                            <Col>
-                                {item.type == "range" &&
-                                    <input
-                                        id={id}
-                                        min={0} max={1}
-                                        value={props.state[key]}
-                                        type="range"
-                                        step={0.05}
-                                        onChange={(event) => {
-                                            event.preventDefault();
-                                            props.setState(key, parseFloat(event.target.value));
-                                        }}
-                                    />}
-                                {item.type == "checkbox" &&
-                                    <input id={id}
-                                        type="checkbox"
-                                        checked={props.state[key]}
-                                        onChange={(event) => {
-                                            event.preventDefault();
-                                            props.setState(key, event.target.checked)
-                                        }}
-                                        readOnly />}
-                                {item.type == "select" &&
-                                    <select id={id}
-                                        onChange={(event) => {
-                                            event.preventDefault();
-                                            props.setState(key, event.target.value)
-                                        }}>
-                                        {item.options.map(x => <option value={x}>{x}</option>)}
-                                    </select>}
-                            </Col>
-                        </Row>
-                    )
-                })}
+                {Object.keys(controls).map((key) => <ControlRow key={key} name={key} />)}
+                <Row>
+                    <Col>
+                        <Button
+                            variant="primary"
+                            size="sm"
+                            onClick={() => { props.setState(form) }}>
+                            Submit!
+                        </Button>
+                    </Col>
+                </Row>
             </Card.Body>
-        </Card >)
+        </Card >
+    )
 }
 
 function StarryNightView(props) {
-
-    const acc = (total, [key, value]) => {
-        total[key] = value.default;
-        return total;
-    }
-    const init = {
-        height: window.innerHeight,
-        width: window.innerWidth
-    }
-    const [state, setState] = useState(Object.entries(controls).reduce(acc, init));
-
-    const setKeyValue = (key, value) => {
-        var tmp = {}
-        tmp[key] = value;
-        setState(Object.assign({}, state, tmp));
+    const getDefaultControl = () => {
+        return Object.entries(controls).reduce(
+            (total, [key, value]) => {
+                total[key] = value.default;
+                return total;
+            }, {
+            height: window.innerHeight,
+            width: window.innerWidth
+        });
     }
 
+    const [state, setState] = useState(getDefaultControl());
+
+    /*
     window.onresize = function () {
-        setKeyValue("height", window.innerHeight);
-        setKeyValue("width", window.innerWidth);
-    }
+        setState({
+            height: window.innerHeight,
+            width: window.innerWidth
+        });
+    }*/
+
+    console.log("state", state);
 
     return (
         <Container id="starry-night-view" className="view-root" fluid>
-            <StarryNight state={state} setState={setState} />
-            <StarryNightControl state={state} setState={setKeyValue} />
+            <StarryNight config={state} />
+            <StarryNightControl state={state} setState={setState} />
         </Container>
     )
 }
